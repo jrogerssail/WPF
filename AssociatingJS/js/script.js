6e04d026f238d016b0f51827d9ff7a94f62bc08f
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-10 16:38:35 -0400

    Revert "Create gh-pages branch via GitHub"
    This reverts commit 859cfd1e754b1bf3150d3be0f88816f091407c77.

---
+++ AssociatingJS/js/script.js
@@ -0,0 +1,18 @@
+alert("This is in my JS file");
+var yearBorn = 1955;
+console.log("Hello World!");
+console.log(yearBorn);
+
+var age;
+age = 38;
+
+var yearOfBirth = 1971;
+
+var isStudent = false;
+
+var firstName = "Jason";
+
+var phrase = 'I don\'t know';
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
+console.log(muppetNames[0]);
