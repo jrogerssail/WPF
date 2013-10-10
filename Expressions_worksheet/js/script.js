48dbfbb531ebc7d30c82a754700a8f6e238ce46d
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-10 18:36:01 -0400

    circumference

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,1 +1,9 @@
 /* Lewie "Jason" Rogers, 10/10/2013, Expression worksheet */
+//Calculate the Circumference of a circle
+var radius = 6;
+const PI = 3.14159265
+
+//multiply 2, PI and the radius & assign to circ variable
+var circ = radius * 2 * PI;
+//Print out the output Circumference
+console.log("The Circumference of the circle is " + circ)
