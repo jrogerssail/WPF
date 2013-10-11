df5af297f90f6b6c9ad3d989f37254b2faced4d1
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-10 21:53:44 -0400

    Expressions_Personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,8 @@
-/* Lewie "Jason" Rogers, 10/10/2013, Expression_Personal worksheet */
+//Lewie "Jason" Rogers, 10/10/2013, Expression_Personal
+
+//Finding the area of a bedroom to re-tile
+var width = prompt("What is the width of the room"); //Room width
+var length = prompt("What is the length of the room");//Room length
+var area = width * length;
+alert(" The area of your room is " + area + " sq feet")
+//console.log("area");
--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,4 +1,18 @@
 /* Lewie "Jason" Rogers, 10/10/2013, Expression worksheet */
+//Area of a rectangle
+//alert("What is the width");
+//var width =  6;
+//alert("What is the height");
+//var height = 10;
+//var area = width * height;
+//alert("Your area is ")
+//console.log("The area of the rectangle is " + area)
+
+var width = prompt("What is the width");
+var height = prompt("What is the height");
+var area = width * height;
+console.log("area");
+
 //Calculate the Circumference of a circle
 var radius = 6;
 const PI = 3.14159265
@@ -6,4 +20,4 @@ const PI = 3.14159265
 //multiply 2, PI and the radius & assign to circ variable
 var circ = radius * 2 * PI;
 //Print out the output Circumference
-console.log("The Circumference of the circle is " + circ)
+console.log("The Circumference of the circle is " + circ);
