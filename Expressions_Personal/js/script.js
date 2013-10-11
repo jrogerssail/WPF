52b50139aee0b42389104c2994354386532b49a7
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-10 22:02:46 -0400

    Personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,6 +3,6 @@
 //Finding the area of a bedroom to re-tile
 var width = prompt("What is the width of the room"); //Room width
 var length = prompt("What is the length of the room");//Room length
-var area = width * length;
-alert(" The area of your room is " + area + " sq feet")
+var area = width * length;//Formula for area
+alert(" The area of your room is " + area + " sq feet")//Answer to user inputs
 //console.log("area");
