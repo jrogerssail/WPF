2a5526f783967b77695293fcbdc1bcd4438cdb07
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-17 11:48:04 -0400

    Stuff Your Face

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -2,16 +2,24 @@
 
 //Hot Enough?
 //Is it hot enough to go to beach?
-//var myTemp = prompt("What is the current temperature?");
-var myTemp = prompt("What is the current temp?"); //Input the current temp
-var temp = 80;
-//if the temperature is less than 75
-if(myTemp < 75){
+//var myTemp = prompt("What is the current temp?"); //Input the current temp
+////var temp = 80; //Not used in formula
+//if(myTemp < 75){
 	//if its less than 75
-	console.log("We will go to the beach!");
-}else{
+	//console.log("We will go to the beach!");
+//}else{
 	//if it's greater or equal to 75
-	console.log("We will go to movies.");
+	//console.log("We will go to movies.");
+//}
+
+//Stuff Your Face
+var myWeight = prompt("What is your current weight"); //Competitors Weight
+if(myWeight < 250){
+	//if its less than 250
+	console.log("The competitor needs to gain some weight!");
+}else{
+	//if it's greater or equal to 250
+	console.log("The competitor qualifies for the heavyweight division.");
 }
 
 
