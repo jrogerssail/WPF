041a3b734c445371002a9cf49349afbe35d526cd
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-17 11:40:08 -0400

    Deleted js2

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,12 +1,21 @@
-//Lewie "Jason" Rogers, 10/10/2013, Expression_Personal
+//Lewie "Jason" Rogers, 10/10/2013, Conditionals_worksheet
 
-//Finding the area of a bedroom to re-tile
-var width = prompt("What is the width of the room in feet?"); //Room width
-var length = prompt("What is the length of the room in feet?");//Room length
-var area = width * length;//Formula to find the sq area of a room
-alert(" The area of your room is " + area + " sq feet")//Answer to user inputs
-var tile = prompt("What is the size of the tile in inches?");
-var convert = tile/12;
-var final = area/convert;//Formula to find the sq area of a room
-alert(" You need " + final + " tiles")//Answer to user inputs
-console.log(final);//JS Console
+//Hot Enough?
+//Is it hot enough to go to beach?
+//var myTemp = prompt("What is the current temperature?");
+var myTemp = prompt("What is the current temp?"); //Input the current temp
+var temp = 80;
+//if the temperature is less than 75
+if(myTemp < 75){
+	//if its less than 75
+	console.log("We will go to the beach!");
+}else{
+	//if it's greater or equal to 75
+	console.log("We will go to movies.");
+}
+
+
+
+
+
+
