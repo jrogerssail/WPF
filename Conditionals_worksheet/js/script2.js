f0a25a5b458c8158a6f9d202873d4b2b68cfd924
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-17 11:03:32 -0400

    Conditionals_worksheet

---
+++ Conditionals_worksheet/js/script2.js
@@ -0,0 +1,9 @@
+//Lewie "Jason" Rogers, 10/10/2013, Expression_Personal
+
+//Finding the area of a bedroom to re-tile
+var firstName = prompt("What is the width of the room in feet"); //Room width
+var middleName = prompt("What is the length of the room in feet");//Room length
+var lastName = prompt("What is the length of the room in feet");//Room length
+var fullName = firstName + " " + middleName + " " + lastName;//Formula for area
+alert("Thank you " + fullName + " for signing up")//Answer to user inputs
+console.log(fullName);//JS Console
