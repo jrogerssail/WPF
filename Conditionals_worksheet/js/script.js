cdf66472fa4a08ba94c5e681bc65529c6e0d3adb
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-17 13:34:06 -0400

    Starting Login

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -22,24 +22,11 @@ if(myWeight < 250){
 	console.log("The competitor qualifies for the heavyweight division.");
 }
 
-////////////function calcMPG() {
-//////////var startMiles;
-////////var endMiles;
-//////var gallons;
-////if (alert(isNAN())){
-//d//ocument.write("You must enter a number");
-//}else {
-//(startMiles>0) && (endMiles>0) && (gallons>0)
-//answer = (endMiles-startMiles)/gallons;
-//console.log(answer);
-//}
-
-
-//LAST CHANCE FOR GAS
+//GROUP 1 - LAST CHANCE FOR GAS
 var myMPG = prompt("What is the MPG of your car?"); //Drivers MPG
 var myTank = prompt("What is gas gauge reading in PERCENT?");//Gas in tank
 var carTank = prompt("How many gallons does your tank hold?");//Max gallons of tank
-var canI = myMPG * carTank/myTank;
+var canI = myMPG * carTank/myTank;//Final formula
 if(canI < 200){
 	//if its less than 200
 	console.log("You only have X gallons of gas in your tank, better get gas now while you can!");
@@ -48,6 +35,21 @@ if(canI < 200){
 	console.log("Yes, you can make it without stopping for gas!");
 }
 
+//GROUP 2 - CHECK THE LOGIN
+var userName = Array("Jason", "Mason", "Matthew", "Colette", "Abby", "Baylee" );
+var myMPG = prompt("What is the MPG of your car?"); //Drivers MPG
+var myTank = prompt("What is gas gauge reading in PERCENT?");//Gas in tank
+var carTank = prompt("How many gallons does your tank hold?");//Max gallons of tank
+var canI = myMPG * carTank/myTank;//Final formula
+if(canI < 200){
+	//if its less than 200
+	console.log("You only have X gallons of gas in your tank, better get gas now while you can!");
+}else{
+	//if it's greater or equal to 200
+	console.log("Yes, you can make it without stopping for gas!");
+}
+var unArray = ["Philip", "George", "Sarah", "Michael"];  // as many as you like - no comma after final entry
+var pwArray = ["Password1", "Password2", "Password3", "Password4"];  // the corresponding passwords;
 
 
 
