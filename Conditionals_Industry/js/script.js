d50a368b20c827c308a7019379f7f32014f153a9
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-17 23:47:01 -0400

    Conditionals_Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,15 +4,15 @@
 var onlineSales = prompt("Total number of sales.");//Total number of at-bats
 var discount = onlineSales * .20;//Number of basehits
 var totalSales = discount;//Walks are subtracted from at-bats
+//var extraDiscount = 0.05;
+var maxDiscount = totalSales * extraDiscount;
 if(totalSales > 500){
 	//if its less than .280
 	alert("Your discount is $" + totalSales);
 	console.log("Thank you for your order.");
 }else{
-	var extraDiscount = 0.05;
-	(totalSales * extraDiscount);
-	alert("Your balance is $" + totalSales);
-	console.log("Your discount is $" + totalSales);
+	alert("Your discount is $" + maxDiscount);
+	console.log("Your discount is $" + maxDiscount);
 }
-alert("Your discount is $" + totalSales);//Gives player results
+//alert("Your discount is $" + totalSales);//Gives player results
 //console.log(else);//Displays batting average in console
