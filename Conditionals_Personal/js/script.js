4c9e299ae5914226145e841f2f7bd7d6bb25a435
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-17 22:50:10 -0400

    Conditionals_Personal - Final

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,4 +7,10 @@ var walks = prompt("How many walks have you earned");//Walks are subtracted from
 var totalAtBats = atBats - walks;//Finding number of at-bats for batting average
 var battingAverage = baseHits /totalAtBats;//Actual batiing average
 alert("Your batting average is " + battingAverage);//Gives player results
+if(battingAverage < .280){
+	//if its less than .280
+	console.log("Sorry you did not qualify for All-Stars.");
+}else{
+	console.log("You qualify for All-Stars!");
+}
 console.log(battingAverage);//Displays batting average in console
