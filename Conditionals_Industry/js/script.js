24869c814ab3c81f36f56370f95e0c77060fd9a3
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-17 23:14:49 -0400

    Conditionals_Personal - resave

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,16 +1,17 @@
-//Lewie "Jason" Rogers, 10/10/2013, Conditionals_Personal
+//Lewie "Jason" Rogers, 10/10/2013, Conditionals_Industry
 
 //What is my batting average?
-var atBats = prompt("Enter number of at-bats.");//Total number of at-bats
-var baseHits = prompt("Enter number of basehits");//Number of basehits
-var walks = prompt("How many walks have you earned");//Walks are subtracted from at-bats
-var totalAtBats = atBats - walks;//Finding number of at-bats for batting average
-var battingAverage = baseHits /totalAtBats;//Actual batiing average
-alert("Your batting average is " + battingAverage);//Gives player results
-if(battingAverage < .280){
+var onlineSales = prompt("Total number of sales.");//Total number of at-bats
+var discount = onlineSales * .2;//Number of basehits
+var totalSales = prompt("Did you sale more than $500?");//Walks are subtracted from at-bats
+//var totalAtBats = atBats - walks;//Finding number of at-bats for batting average
+//var battingAverage = baseHits /totalAtBats;//Actual batiing average
+alert("Your discount is $" + totalSales);//Gives player results
+if(totalSales < 500){
 	//if its less than .280
-	console.log("Sorry you did not qualify for All-Stars.");
+	console.log("Thank you for your order.");
 }else{
-	console.log("You qualify for All-Stars!");
+	(totalSales * .05);
+	console.log("You qualified for an additional discount");
 }
-console.log(battingAverage);//Displays batting average in console
+console.log(totalSalesDiscount);//Displays batting average in console
--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,4 +1,4 @@
-//Lewie "Jason" Rogers, 10/10/2013, Conditionals_Industry
+//Lewie "Jason" Rogers, 10/10/2013, Conditionals_Personal
 
 //What is my batting average?
 var atBats = prompt("Enter number of at-bats.");//Total number of at-bats
@@ -8,9 +8,9 @@ var totalAtBats = atBats - walks;//Finding number of at-bats for batting average
 var battingAverage = baseHits /totalAtBats;//Actual batiing average
 alert("Your batting average is " + battingAverage);//Gives player results
 if(battingAverage < .280){
-	//if its less than .280
+//if its less than .280
 	console.log("Sorry you did not qualify for All-Stars.");
-}else{
+ }else{
 	console.log("You qualify for All-Stars!");
-}
-console.log(battingAverage);//Displays batting average in console
+	}
+	console.log(battingAverage);//Displays batting average in console
