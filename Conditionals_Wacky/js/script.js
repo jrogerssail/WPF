263750dcb5bce2488fd0ccd26f0b00b91dc8f35d
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-18 23:26:39 -0400

    fishType

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,8 +1,16 @@
 //Lewie "Jason" Rogers, 10/10/2013, Conditionals_Wacky
 
-//What is my batting average?
-var onlineSales = prompt("Total number of sales.");//Total sales
-var discount = onlineSales * .20;//affiliate discount
+//Fish size?
+var whatFish = prompt("What kind of fish do you have?");//Total sales
+var fishType = new Array("Oscar, Guppy, Molly, Swords, African Cichlids");
+if(fishType = ""{
+	//if its less than $500
+	alert("Your fish is an aggressive species" + fishType);//Shows balance after first discount and finishes order
+	console.log("Please keep your fish in at least a 20 gallon aquarium.");//Ending script
+}else{
+	alert("Your fish is a community fish and can be kept with other fish of this type" + fishType);//Max discount is applied and shown
+	console.log("Your final balance is $" + finalBill);//Final cost
+}
 alert("Your discount is $" + discount);//Announces your discount
 var creditSales = onlineSales - discount;//Calculates new var after discount is applied
 var extraDiscount = 0.05;//extra discount for sales over $500
@@ -15,4 +23,13 @@ if(creditSales < 500){
 }else{
 	alert("Your final balance is $" + finalBill);//Max discount is applied and shown
 	console.log("Your final balance is $" + finalBill);//Final cost
+}
+
+
+if(document.fishType.name.value =="fishType"){
+var name=document.fishType.name
+name.value="";
+alert("What kind of fish do you have?");
+name.focus();
+return false;
 }
