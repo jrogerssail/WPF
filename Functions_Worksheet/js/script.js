873e093b507d1a4e17f94f645bfaff3e4608072a
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-23 18:03:58 -0400

    Functions_Worksheet - Bee stings

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -10,6 +10,16 @@ var circle = circumCircle(3.14, 6);
 
 console.log("The circumference of the circle is " + circle);
 
+//Bee Stings
+var beeStings = function(vw, bs){
+	var death = 185 * 8.666666667;
+	return death;
+}
+
+var finalStings = beeStings(185, 8.666666667);
+
+console.log("It takes " + finalStings + " bee stings to kill this animal");
+
 
 
 
