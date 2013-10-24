523a592a408b9068165177f41e178ef9dbfb1359
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-24 16:19:38 -0400

    Functions_Industry - tv

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -17,6 +17,23 @@ console.log("The pitchers ERA is " + pitchersEra); //console to see results
 
 //index.html page runs as exxpected when script is loaded
 
+var tvSize = prompt("What size TV are you looking to purchase?");
+var tvStock = 27; 		//sets up index
+while(tvStock > 0){		//checks condition
+	console.log(tvStock + 'These are you TV choices');
+	tvStock --; //increments or decrements the index
+}
+
+console.log('---------- Do While Loops ----------');
+
+var c = 10;
+do{
+	console.log(c + 'Kegs on the wall');
+	c --;
+}
+while(c > 27);
+
+console.log('---------- For Loops ----------')
 
 
 
