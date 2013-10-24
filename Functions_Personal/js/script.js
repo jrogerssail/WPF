7c6c3958aeb970674afd44c196100478d6c70bc1
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-24 10:27:41 -0400

    Functions_Personal - Start

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,25 @@
+//Lewie "Jason" Rogers, 10/21/2013, Functions_Worksheet
+
+//Circumference
+var circumCircle = function(p, d){
+	var circumference = 3.14 * 6;
+	return circumference;
+}
+
+var circle = circumCircle(3.14, 6);
+
+console.log("The circumference of the circle is " + circle);
+
+//Bee Stings
+var beeStings = function(vw, bs){
+	var death = 185 * 8.666666667;
+	return death;
+}
+
+var finalStings = beeStings(185, 8.666666667);
+
+console.log("It takes " + finalStings + " bee stings to kill this animal");
+
+
+
+
