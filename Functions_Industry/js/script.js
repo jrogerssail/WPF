708594d0ddbd8f1204981fd6ddb6528f2783d238
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-24 16:20:39 -0400

    Functions_Industry - tv update

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,23 +1,7 @@
 //Lewie "Jason" Rogers, 10/21/2013, Functions_Industry
 
 //Average TV Size
-var earnedRuns = prompt("How many earned runs were charged to the pitcher?");//Total number of earned runs charged to the pitchers
-var inningsPitched = prompt("How many innings did the pitcher pitch?");//Total number of innings pitched by the pitcher
-var era = function(e, i){ //Function for finding ERA or How many runs the pitcher gives up for every 9 innings pitched.
-	var completeGame = 9; //Number of innings for complete game
-	var era = (e / i) * completeGame; //Formula for finding ERA
-	return era; //Returns Result
-}
-
-var pitchersEra = era(earnedRuns, inningsPitched); //Variable to be output
-
-console.log("The pitchers ERA is " + pitchersEra); //console to see results
-
-//(earned runs / innings pitched) x 9 for number of innings per complete game = ERA or Earned Run Average
-
-//index.html page runs as exxpected when script is loaded
-
-var tvSize = prompt("What size TV are you looking to purchase?");
+var tvSize = prompt("What size TV are your looking to purchase?");
 var tvStock = 27; 		//sets up index
 while(tvStock > 0){		//checks condition
 	console.log(tvStock + 'These are you TV choices');
