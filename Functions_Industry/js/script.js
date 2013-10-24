d35ab4c41ea2073bf83dd4840924cefae7e143a9
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-24 17:00:31 -0400

    Functions_Industry next step

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,24 +2,20 @@
 
 //Average TV Size
 var tvSize = prompt("What size TV are your looking to purchase?");
-var tvStock = 27; 		//sets up index
-while(tvStock > 0){		//checks condition
-	console.log(tvStock + 'These are you TV choices');
+var tvStock = 27 + "inch RCA"; 		//sets up index
+if(tvSize > tvStock){		//checks condition
+	var stockSizes = (22, 24, 26);
+	console.log(stockSizes + 'These are you TV choices');
 	tvStock --; //increments or decrements the index
 }
 
-console.log('---------- Do While Loops ----------');
-
-var c = 10;
-do{
-	console.log(c + 'Kegs on the wall');
-	c --;
-}
-while(c > 27);
-
-console.log('---------- For Loops ----------')
+console.log("These are you TV choices " + tvStock);
 
 
+
+//console.log('---------- For Loops ----------')
+
+//tvChoices(22inch RCA, 22inch Samsung, 24inch RCA);
 
 
 
