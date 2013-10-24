0d335f583cd6940d4460f20ee6aca94b03fe69cf
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-24 19:00:44 -0400

    Functions_Industry - closing

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -12,8 +12,21 @@ if(tvSize > tvStock){		//checks condition
 	var tv32 = 32;
 	var tv36 = 36;
 	var tv42 = 42;
-	alert("These are your TV choices... " + stockSizes + tv32 + " inch RCA, " + tv36 + " inch Vizio, " + tv42 + "inch Samsung");
-	console.log('These are your TV choices... ' + stockSizes);
+	alert("These are your TV choices... " + stockSizes + tv32 + " inch RCA, " + tv36 + " inch Vizio, " + tv42 + " inch Samsung");
+	console.log("These are your TV choices... " + stockSizes + tv32 + " inch RCA, " + tv36 + " inch Vizio, " + tv42 + " inch Samsung");
+	tvStock --; //increments or decrements the index
+}
+if(tvSize < tvStock){		//checks condition
+	var stockSizes = 27 + " inch RCA, ";
+	var tv22 = 22;
+	var tv24 = 24;
+	var tv26 = 26;
+	var tv27 = 27;
+	var tv32 = 32;
+	var tv36 = 36;
+	var tv42 = 42;
+	alert("These are your TV choices... " + stockSizes + tv22 + " inch RCA, " + tv24 + " inch Vizio, " + tv26 + " inch Samsung");
+	console.log("These are your TV choices... " + stockSizes + tv22 + " inch RCA, " + tv24 + " inch Vizio, " + tv26 + " inch Samsung");
 	tvStock --; //increments or decrements the index
 }
 //console.log("These are you TV choices " + tvStock);
