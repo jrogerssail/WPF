56d22b6a704e1d56b8826e1ed38fa9b8bf1e6d18
Jason Rogers <jrogerssail@fullsail.edu>
2013-10-24 20:17:51 -0400

    work in prohgress

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,18 +1,22 @@
 //Lewie "Jason" Rogers, 10/21/2013, Functions_Wacky
 
-//Average TV Size
-var tvSize = prompt("What size TV are your looking to purchase?"); //Ask user question
-var tvStock = 27 + " inch RCA"; 		//sets up index
-if(tvSize > tvStock){		//checks condition
-	var stockSizes = 30 + " inch RCA, ";
-	var tv22 = 22;
-	var tv24 = 24;
-	var tv26 = 26;
-	var tv27 = 27;
-	var tv32 = 32;
-	var tv36 = 36;
-	var tv42 = 42;
-	alert("These are your TV choices... " + stockSizes + tv32 + " inch RCA, " + tv36 + " inch Vizio, " + tv42 + " inch Samsung");
+//Halloween Candy
+var hershey = prompt("How many pieces are in the bag?"); //Ask user question
+var skittles = prompt("How many pieces are in the bag?"); //Ask user question
+var reeses = prompt("How many pieces are in the bag?"); //Ask user question
+var smarties = prompt("How many pieces are in the bag?"); //Ask user question
+var snickers = prompt("How many pieces are in the bag?"); //Ask user question
+var mars = prompt("How many pieces are in the bag?"); //Ask user question
+var bagSize = 20 + " Halloween bags"; 		//sets up index
+var candyType = hershey, skittles, reeses, smarties, snickers, mars;
+if(candyType > 18){		//checks condition
+	var extraHershey = hershey - bagSize + " extra pieces, ";
+	var extraSkittles = skittles - bagSize + " extra pieces, ";
+	var extraReeses = reeses - bagSize + " extra pieces, ";
+	var extraSmarties = smarties - bagSize + " extra pieces, ";
+	var extraSnickers = snickers - bagSize + " extra pieces, ";
+	var extraMars = mars - bagSize + " extra pieces, ";
+	alert("You will have " + stockSizes + extraHershey + "Hersheys" + extraSkittles + " Skittles, " + extraReeses + " Reese's" + extraSmarties + " Smarties" + extraSnickers + " Snickers" + extraMars + " Mars");
 	console.log("These are your TV choices... " + stockSizes + tv32 + " inch RCA, " + tv36 + " inch Vizio, " + tv42 + " inch Samsung");
 	tvStock --; //increments or decrements the index
 }
